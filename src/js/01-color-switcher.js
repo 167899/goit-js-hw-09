b7f1a17fa003d9195a83866b71d7b4ac73ac7dc3
Antonenko <42521426+167899@users.noreply.github.com>
2022-05-25 16:01:30 +0300

    part - 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let startId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onClick = () => {
+  startId = setInterval(() => {
+    body.setAttribute('style', `background-color:${getRandomHexColor()}`);
+  }, 1000);
+  btnStart.removeEventListener('click', onClick);
+};
+
+btnStart.addEventListener('click', onClick);
+
+btnStop.addEventListener('click', () => {
+  clearInterval(startId);
+  btnStart.addEventListener('click', onClick);
+});
